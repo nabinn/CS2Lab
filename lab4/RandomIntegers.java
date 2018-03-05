dc8152ab24420f7d9ec06362571f62b3befdb816
Nabin <imnabn@gmail.com>
2018-03-05 00:17:48 -0600

    Update RandomIntegers.java

--- lab4/RandomIntegers.java
+++ lab4/RandomIntegers.java
@@ -4,7 +4,7 @@ Creates an array with 100 randomly chosen integers.
 Prompts the user to enter the index of the array. Then display the corresponding element
 value. If the specified index is out of bounds, display the message Out of Bounds.
  */
-package lab4;
+//package lab4;
 
 import java.util.Scanner;
 
