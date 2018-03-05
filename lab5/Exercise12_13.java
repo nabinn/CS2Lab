1a89be798b79bc6db97527e950cc3de65258b500
nabinn <imnabn@gmail.com>
2018-03-04 23:35:55 -0600

    update lab 5

---
+++ lab5/Exercise12_13.java
@@ -0,0 +1,66 @@
+package lab5;
+
+/**
+ Write a program that will count the number of characters, words, and lines
+ in a file. Words are separated by whitespace characters. 
+ The file name should be passed as a command-line argument.
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercise12_13 {
+	
+	public static void main(String[] args) {
+		
+		if (args.length < 1) {
+			System.out.println("Usage: java Exercise12_13 filename");
+			System.exit(1);
+		}
+		
+		// filename is provided as first command line argument
+		String fileName = args[0];
+		File file = new File(fileName);
+		if (!file.exists()) {
+            System.out.println(file + " does not exist.");
+            System.exit(1);
+        }
+		
+		int numChars = 0; 
+        int numWords = 0; 
+        int numLines = 0;
+
+        try {
+        	
+            Scanner input = new Scanner(file);
+      
+            // while there is next line in the file
+            while (input.hasNextLine()) {
+            	//increment line count
+            	numLines++;
+            	
+            	// read the next line
+            	String line = input.nextLine();
+                
+            	// add line length to number of characters
+            	numChars += line.length();
+                
+                // split the line by space to count words
+                String[] wordArray = line.split(" ");
+                numWords += wordArray.length;
+            }
+            
+            input.close();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    
+        System.out.println("Number of Characters: " + numChars);
+        System.out.println("Number of Words: " + numWords);
+        System.out.println("Number of Lines: " + numLines);
+
+
+    }
+
+}
---
+++ lab5/Exercise12_15.java
@@ -0,0 +1,70 @@
+package lab5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+Write a program to create a file named Exercise12_15.txt if it does not exist. 
+Write 100 integers created randomly into the file using text I/O.
+Integers are separated by spaces in the file. Read the data back from the file and
+display the data in increasing order.
+*/
+
+public class Exercise12_15 {
+	
+	public static void main(String[] args) {
+		
+		File file = new File("Exercise12_15.txt");
+		
+		// if file doesn't exit, create it
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// write 100 random integers to the file separated by space
+		try {
+			PrintWriter output = new PrintWriter(file);
+			
+			for (int i = 0 ; i < 100 ; i++ ) {
+				// range of random integers : 0 to 999
+				output.print(((int)(Math.random() * 1000)) + " ");
+			}
+			output.close();
+			System.out.println("Write Complete");
+		} catch(FileNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		
+		
+		//read data back from the file and put it in a list
+		List<Integer> listOfNumbers = new ArrayList<Integer>(); 
+		try {
+			Scanner input = new Scanner(file);
+			while (input.hasNext()) {
+				listOfNumbers.add(input.nextInt());
+			}
+			input.close();
+		}catch(FileNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		
+		// sort the list, so that numbers are in increasing order
+		Collections.sort(listOfNumbers); // sorts the list in-place
+		
+		
+		// print the numbers in sorted list
+		System.out.println(listOfNumbers);
+		
+	}
+	
+}
